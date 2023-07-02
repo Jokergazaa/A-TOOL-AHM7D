18df49300cab2f0a1589034ed8515ef88bc565e9
الجوكر الفلسطيني <107787017+Jokergazaa@users.noreply.github.com>
2023-07-02 15:51:15 +0300

    Add files via upload

---
+++ about_us.sh
@@ -0,0 +1,13 @@
+echo " "
+echo " "
+clear
+blue='\033[1;32m' 
+sleep 2
+ echo "                      Coded BY >>>>> AHM7D >>>>>" |lolcat
+sleep 1
+echo "                      F.B Channel: AHM7D Sy" |lolcat
+sleep 1
+echo -e "[ ✔ ] Follow Us on instagram : https://www.instagram.com/joker.plstaeen/"
+echo -e "[ ✔ ] HAPPY HACKING ...DON'T FORGOT TO SUBSCRIBE"
+sleep 3
+bash main_script.sh
